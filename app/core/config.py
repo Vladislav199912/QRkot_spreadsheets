f93930f7c8afa14d77084a4220db1c7fba3dc589
Владислав Ермаков <vladermakov@mail.ru>
2024-03-03 22:28:58 +0500

    Check6

--- app/core/config.py
+++ app/core/config.py
@@ -1,16 +1,39 @@
-from pydantic import BaseSettings
+import logging
+import sys
+from typing import Optional
 
-from app.core.constants import APP_INFO, APP_TITLE, DATABASE_URL, SECRET
+from pydantic import BaseSettings, EmailStr
 
 
 class Settings(BaseSettings):
-    app_title: str = APP_TITLE
-    app_info: str = APP_INFO
-    database_url: str = DATABASE_URL
-    secret: str = SECRET
+    app_title: str = 'Кошачий благотворительный фонд (0.1.0)'
+    description: str = 'Сервис для поддержки котиков!'
+    database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
+    secret: str = 'SECRET'
+    first_superuser_email: Optional[EmailStr] = None
+    first_superuser_password: Optional[str] = None
+
+    type: Optional[str] = None
+    project_id: Optional[str] = None
+    private_key_id: Optional[str] = None
+    private_key: Optional[str] = None
+    client_email: Optional[str] = None
+    client_id: Optional[str] = None
+    auth_uri: Optional[str] = None
+    token_uri: Optional[str] = None
+    auth_provider_x509_cert_url: Optional[str] = None
+    client_x509_cert_url: Optional[str] = None
+    email: Optional[str] = None
 
     class Config:
         env_file = '.env'
 
 
 settings = Settings()
+
+logging.basicConfig(
+    level=logging.INFO,
+    stream=sys.stdout,
+    format=('%(asctime)s | %(levelname)s | %(message)s '
+            '| %(filename)s:%(lineno)d '),
+)
