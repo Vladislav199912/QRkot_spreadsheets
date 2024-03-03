086049f5678666b00535d7c5eb00b238d14fef7e
Владислав Ермаков <vladermakov@mail.ru>
2024-03-03 23:22:37 +0500

    Check

--- app/schemas/charity_project.py
+++ app/schemas/charity_project.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel, Extra, Field, PositiveInt
+from app.constatnts import MAX_LENGTH
 
 
-class CharityProjectBaseSchema(BaseModel):
-    name: Optional[str] = Field(None, max_length=100)
+class CharityProjectBaseSchema(BaseModel, MAX_LENGTH):
+    name: Optional[str] = Field(None, MAX_LENGTH)
     description: Optional[str] = Field(None)
     full_amount: Optional[PositiveInt]
 
@@ -13,8 +14,8 @@ class CharityProjectBaseSchema(BaseModel):
         min_anystr_length = 1
 
 
-class CreateCharityProjectSchema(CharityProjectBaseSchema):
-    name: str = Field(..., max_length=100)
+class CreateCharityProjectSchema(CharityProjectBaseSchema, MAX_LENGTH):
+    name: str = Field(..., MAX_LENGTH)
     description: str = Field()
     full_amount: PositiveInt
 
